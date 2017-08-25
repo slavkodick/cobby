fe3793932ecb0559097a45f560cfe923cd07b837
slavko dick <slavko.dick@mash2.com>
2017-08-25 11:59:29 +0200

    changed copy path

--- dev/travis/before_script.sh
+++ dev/travis/before_script.sh
@@ -20,7 +20,7 @@ mysql -uroot -e 'CREATE DATABASE magento2;'
 php bin/magento setup:install -q --admin-user="admin" --admin-password="123123q" --admin-email="admin@example.com" --admin-firstname="John" --admin-lastname="Doe" --db-name="magento2"
 
 echo "==> Copying the current build to the Magento 2 installation."
-cp -R ../magento-ce/* vendor/slavkodick/cobby/
+cp -R ../vendor/* magento-ce/vendor
 
 # enable the extension, do other relavant mage tasks.
 echo "==> Enable extension, do mage tasks..."
