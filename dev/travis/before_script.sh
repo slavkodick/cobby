7e3eca5f8873b01da18348aa622f85c67bff6e63
slavko dick <slavko.dick@mash2.com>
2017-08-25 12:17:30 +0200

    disabled copy

--- dev/travis/before_script.sh
+++ dev/travis/before_script.sh
@@ -19,8 +19,8 @@ echo "==> Installing Magento 2"
 mysql -uroot -e 'CREATE DATABASE magento2;'
 php bin/magento setup:install -q --admin-user="admin" --admin-password="123123q" --admin-email="admin@example.com" --admin-firstname="John" --admin-lastname="Doe" --db-name="magento2"
 
-echo "==> Copying the current build to the Magento 2 installation."
-cp -R ../vendor/* /vendor/
+#echo "==> Copying the current build to the Magento 2 installation."
+#cp -R ../vendor/* /vendor/
 
 # enable the extension, do other relavant mage tasks.
 echo "==> Enable extension, do mage tasks..."
