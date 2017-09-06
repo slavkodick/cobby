539d1e8110bcb78a302c78ec7b109ccf33b83d68
slavko dick <slavko.dick@mash2.com>
2017-09-06 10:29:04 +0200

    refoctoring

--- dev/travis/script.sh
+++ dev/travis/script.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
+trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
+
 echo '==> Doing phpcs tests with EcgM2 Standard setting.'
 ../magento-ce/vendor/bin/phpcs --standard=../magento-ce/vendor/magento-ecg/coding-standard/EcgM2/ ../magento-ce/vendor/slavkodick/cobby/;  #--standard=;
 
